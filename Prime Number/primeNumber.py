6f2f66243e6ca7bfa20ba2b6a0bb8cf3e80140f8
Soumyajit7 <35652969+Soumyajit7@users.noreply.github.com>
2022-01-02 19:28:24 +0530

    Create primeNumber.py

---
+++ Prime Number/primeNumber.py
@@ -0,0 +1,10 @@
+x = int(input("x = "))
+count = 0
+for i in range(1,x+1):
+    rem = x % i
+    if rem==0:
+        count+=1
+if count<=2:
+    print("prime number")
+else:
+    print("not prime number")
